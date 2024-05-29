0067c7b252dab6ac60c2b7a1c08e36b3431bce1c
oskar lindqvist <lindqvist_11@hotmail.com>
2024-05-30 00:08:54 +0700

    removed unnecessary code

--- Infrastructure/Services/CourseService.cs
+++ Infrastructure/Services/CourseService.cs
@@ -58,64 +58,6 @@ public class CourseService(IDbContextFactory<DataContext> contextFactory) : ICou
         return courseEntities.Select(CourseFactory.Create);
     }
 
-    //public async Task<Course> UpdateCourseAsync(CourseUpdateRequest request)
-    //{
-    //    //await using var context = _contextFactory.CreateDbContext();
-    //    //var existingCourse = await context.Courses.FirstOrDefaultAsync(x =>x.Id == request.Id);
-    //    //if (existingCourse == null) return null!;
-
-    //    //var updatedCourseEntity = CourseFactory.Update(request);
-    //    //updatedCourseEntity.Id = existingCourse.Id;
-    //    //context.Entry(existingCourse).CurrentValues.SetValues(updatedCourseEntity);
-
-    //    //await context.SaveChangesAsync();
-    //    //return CourseFactory.Create(existingCourse);
-
-    //    await using var context = _contextFactory.CreateDbContext();
-    //    var existingCourse = await context.Courses
-    //        .Include(c => c.Authors)
-    //        .Include(c => c.Prices)
-    //        .Include(c => c.Content)
-    //        .Include(c => c.ProgramDetails)
-    //        .FirstOrDefaultAsync(x => x.Id == request.Id);
-
-    //    if (existingCourse == null) return null!;
-
-    //    // Update scalar properties
-    //    context.Entry(existingCourse).CurrentValues.SetValues(request);
-
-    //    // Authors
-    //    existingCourse.Authors!.Clear();
-    //    existingCourse.Authors.AddRange(request.Authors!.Select(a => new AuthorEntity { Name = a.Name }));
-
-    //    // Prices
-    //    if (request.Prices != null)
-    //    {
-    //        existingCourse.Prices!.Currency = request.Prices.Currency;
-    //        existingCourse.Prices.Price = request.Prices.Price;
-    //        existingCourse.Prices.Discount = request.Prices.Discount;
-    //    }
-
-    //    // Content
-    //    if (request.Content != null)
-    //    {
-    //        context.Entry(existingCourse.Content!).CurrentValues.SetValues(request.Content);
-    //    }
-
-    //    if (request.ProgramDetails != null)
-    //    {
-    //        existingCourse.ProgramDetails!.Title_1 = request.ProgramDetails.Title_1;
-    //        existingCourse.ProgramDetails.Description_1 = request.ProgramDetails.Description_1;
-    //        existingCourse.ProgramDetails.Title_2 = request.ProgramDetails.Title_2;
-    //        existingCourse.ProgramDetails.Description_2 = request.ProgramDetails.Description_2;
-    //        existingCourse.ProgramDetails.Title_3 = request.ProgramDetails.Title_3;
-    //        existingCourse.ProgramDetails.Description_3 = request.ProgramDetails.Description_3;
-    //    }
-
-    //    await context.SaveChangesAsync();
-    //    return CourseFactory.Create(existingCourse);
-    //}
-
     public async Task<Course> UpdateCourseAsync(CourseUpdateRequest request)
     {
         await using var context = _contextFactory.CreateDbContext();
